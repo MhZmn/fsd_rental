a0462a5d31518520906514bf3885218f9e288b34
MhZmn <102331031+MhZmn@users.noreply.github.com>
2026-02-19 15:56:23 +0330

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,5 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
-description 'Written by AliReza_At'
+description "Developed By FiveM Scripts Development"
 
-
-client_scripts {
-	"config.lua",
-	"client.lua"
-}
+client_scripts {"config.lua", "client.lua"}
